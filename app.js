6fb92a600a1ce2c7d1ef59d0f8c5d22c8a127978
1 <1>
2021-03-31 20:17:47 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,65 @@
+var createError = require('http-errors');
+var express = require('express');
+var multer = require('multer');
+var ejs = require('ejs');
+var path = require('path');
+var logger = require('morgan');
+var fs = require('fs');
+var rimraf = require('rimraf');
+
+
+var indexRouter = require('./routes/index');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  console.log(err)
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+var uploadsDir = __dirname + '/public/uploads';
+
+fs.readdir(uploadsDir, function(err, files) {
+  files.forEach(function(file, index) {
+    fs.stat(path.join(uploadsDir, file), function(err, stat) {
+      var endTime, now;
+      if (err) {
+        return console.error(err);
+      }
+      now = new Date().getTime();
+      endTime = new Date(stat.ctime).getTime() + ;
+      if (now > endTime) {
+        return rimraf(path.join(uploadsDir, file), function(err) {
+          if (err) {
+            return console.error(err);
+          }
+          console.log('successfully deleted');
+        });
+      }
+    });
+  });
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var multer = require('multer');
+var path = require('path');
+var router = express.Router();
+const uuid = require('uuid');
+const exec = require('child_process').exec;
+
+ function  execShellCommand(cmd) {
+  return new Promise((resolve, reject) => {
+   exec(cmd, (error, stdout, stderr) => {
+    if (error) {
+     console.warn(error);
+    }
+    resolve(stdout? stdout : stderr);
+   });
+  });
+ }
+// Set The Storage Engine
+const storage = multer.diskStorage({
+  destination: './public/uploads/',
+  filename: function (req, file, cb) {
+    cb(null, uuid.v4().toString());
+  }
+});
+
+// Init Upload
+const upload =  multer({
+  storage: storage,
+  limits: { fileSize: 1000000 },
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  }
+}).single('myImage');
+
+// Check File Type
+function checkFileType(file, cb) {
+  // Allowed ext
+  const filetypes = /json/;
+  // Check ext
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  // Check mime
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb('Error: Images Only!');
+  }
+}
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index');
+});
+
+router.post('/upload',upload, async (req, res) => {
+      if (req.file == undefined) {
+        res.render('index', { msg: "Error : No File Selected" });
+      }
+      else {
+        var cmdGenerateResume = "hackmyresume build ./public/uploads/"+req.file.filename+" TO ./public/"+req.file.filename+"/out/resume.all -t node_modules/jsonresume-theme-daru"
+        await execShellCommand(cmdGenerateResume) ;
+        var cmdGenerateResumePDF = "wkhtmltopdf --margin-left '0mm' --margin-right '0mm' --header-html ./header.html --footer-html ./footer.html ./public/"+req.file.filename+"/out/resume.pdf.html ./public/"+req.file.filename+"/resumefinal.pdf"
+        await execShellCommand(cmdGenerateResumePDF) ;
+        res.render('index', { msg: "Resume created click download", file: `/download/${req.file.filename}` });
+      }
+});
+
+router.get('/download/:id', (req, res) => {
+  req.params.id
+  res.download("./public/"+req.params.id+"/resumefinal.pdf");
+});
+module.exports = router;
